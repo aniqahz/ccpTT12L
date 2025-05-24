347c30a41dc587026ed1d8c53a13bcae6f31711e
hanani <hhanani.iman@gmail.com>
2025-05-24 22:05:58 +0800

    base, think, look, fire, move done draft

--- robot.h
+++ robot.h
@@ -16,9 +16,9 @@
     int remainingLives; 
    
     public:
+    baseRobot(){};
     baseRobot(int x, int y) :PosX(x), PosY(y), isAlive(true), remainingLives(3) {}
     
-
     pair<int,int> getPosition() const {return {PosX,PosY};}
 
       void setPosition(int x,int y)
@@ -34,13 +34,14 @@
  class movingRobot : virtual public baseRobot {
   public:
    movingRobot(int x,int y): baseRobot(x,y) {}
-    virtual void move()=0;
+    virtual void move(int dx, int dy, const vector<vector<char>>& field)=0;
  };
 
  //SHOOTING ROBOT----------------------------------------------------
  class shootingRobot : virtual public baseRobot {
+  public :
    shootingRobot(int x,int y): baseRobot(x,y) {}
-   virtual void fire(int x,int y)=0;
+   virtual void fire(int x,int y, const vector<vector<char>>&field)=0;
  };
 
  //SEEING ROBOT----------------------------------------------------------
@@ -54,7 +55,7 @@
  class thinkingRobot : virtual public baseRobot {
   public:
   thinkingRobot(int x,int y): baseRobot(x,y) {}
-  virtual void think()=0; 
+  virtual void think(const vector<vector<char>>& field)=0; 
  };
 
  //GENERIC ROBOT----------------------------------------------------
@@ -70,8 +71,7 @@ private:
     bool hasSeeingUpgrade;
 
 public:
-    GenericRobot(string robotName, int x, int y)
-    : baseRobot(x, y), movingRobot(x, y), shootingRobot(x, y), lookRobot(x, y), thinkingRobot(x, y);
+    GenericRobot(string robotName, int x, int y): 
         baseRobot(x, y),
         movingRobot(x, y),
         shootingRobot(x, y),
@@ -84,10 +84,44 @@ public:
         upgradesUsed(0),
         hasMovingUpgrade(false),
         hasShootingUpgrade(false),
-        hasSeeingUpgrade(false) {
+        hasSeeingUpgrade(false) 
+    { 
+        robotType = "GenericRobot"; //set the robot type
+        PosX = x; // initial position
+        PosY = y; // initial position
+        isAlive = true; //alive status
     };
 
+    
 
+    //THINK MECHANICS++++++++++++++++++++++++++++++++++++++++++
+    void think(const vector<vector<char>>& field) override { //decides what action to take
+    int action=rand()%5; 
+    int dx = rand() % 3 - 1; //randomly choose direction to fire (generates a number from 0-2 then substracts 1)
+    int dy = rand() % 3 - 1; //randomly choose direction to fire
+    switch(action){
+
+    case 0:
+    cout<<"Moving to a new position..."<<endl;
+    break;
+    case 1: //fire
+    cout<<"DECISION : FIRE!..."<<endl;
+    fire(dx,dy, field);
+    break;
+    case 2: //look
+    cout<<"Looking around for enemies..."<<endl;
+    look(PosX, PosY, field); //look at current position
+    break;
+    case 3: //move
+    cout<<"Moving..."<<endl;
+    move(dx, dy, field); //move in a random direction
+    break;
+    case 4:
+    cout<<"Staying still..."<<endl;
+    break;
+    }};
+
+//LOOKING MECHANICS+++++++++++++++++++++++++++++
     void look(int dx,int dy, const vector<vector<char>>&field) override {
       int newX = PosX + dx;
     int newY = PosY + dy; //(robotsPositionX + x, robotsPositionY + y)
@@ -112,65 +146,67 @@ public:
     else{
       cout<<"Drifting away out of bounds..."<<endl;
     }}
+  //FIRE MECHANICS++++++++++++++++++++++++++++++++++++++++++++
+    void fire(int dx, int dy, const vector<vector<char>>&field) override{
 
-    
-    void think() override { //decides what action to take
-    int action=rand()%4; 
-    switch(action){
-    case 0:
-    cout<<"Moving to a new position..."<<endl;
-    int dx=rand()%3-1; // -1,0,1
-    int dy=rand()%3-1; // -1,0,1
-    PosX+=dx;
-    PosY+=dy;
-    
-    break;
-    case 1: //fire
-    cout<<"Shooting an enemy!..."<<endl;
-    fire()
-    break;
-    case 2: //look
-    cout<<"Looking around for enemies..."<<endl;
-    break;
-    case 3: //move
-    cout<<"Moving..."<<endl;
-    break;
-    case 4:
-    cout<<"Staying still..."<<endl;
-    break;
-    }};
-
-    void fire(int x, int y) override{
+      if (PosX==0 && PosY==0 && PosX==dx && PosY==dy){ //prevents suicide wow #mentalawareness month
+        cout<<"Cannot fire from the origin!"<<endl;
+        return;
+      }
+      for (int dx=-1; dx<=1; dx++){
+        for (int dy=-1;dy<=1;dy++){
+        int targetX= PosX+dx;
+        int targetY = PosY+dy;
       
-      if (shells>0){
-        shells--;
-        cout<<"Firing at position ("<<x<<","<<y<<")"<<endl;}
+      if (shells>0){ //if bot has more bullets
+        shells--; 
+        int probability = rand()%100;
+        if (targetX>= 0 && targetY<field.size()&& targetY>=0 && targetY<field[0].size()){
+          if (field[targetX][targetY] == '.'){
+            if (probability>=70){
+             cout<<"Firing at ("<< targetX<<","<<targetY<<")! KaBaBoom!!"<<endl;
+            }
+            else{
+            cout<<"Missed at ("<< targetX<<","<<targetY<<")!"<<endl;
+            }}
+          else{
+            cout<<"No enemy in range!";
+          };
+        }}
+        
         else{
-        cout<<"Out of shells!"<<endl;
-        }
+      cout<<"Out of shells! Self-destructing now X___X"<<endl;
+        };
       }
 
+    };};
+
+    //MOVE MECHANICS++++++++++++++++++++++++++++++++++++++++++++
+    void move(int dx, int dy, const vector<vector<char>>& field) override {
+      int newX=PosX+dx;
+      int newY=PosY+dy;
+
+      if (newX>=0 && newX<field.size()&&newY>=0 && newX<field[0].size()){ 
+        PosX = newX;
+        PosY = newY;
+        cout<<"Moved to position ("<<PosX<<","<<PosY<<")"<<endl;
+      }
+      else{
+        cout<<"Cannot move out of bounds!"<<endl;
+      }
+      
     }
-};
+
+  };
        
 
 
 //JUMP BOT-------------------------
-/*class jumpBot : public movingRobot, public thinkingRobot{
-   private:
-   string name;
-   int maxX, maxY; //maximum space you can jump (depends on battlefield)
-   bool upgraded;
+class jumpBot : public movingRobot, public thinkingRobot{
+  private :
+  string name;
+  int jumps; //max 3 jumps/match
 
-   public:
-    jumpBot(string robotName, int id, int battlefieldWidth, int battlefieldHeight) {
-      name = robotName;
-      PosX = rand() % battlefieldWidth;
-      PosY = rand() % battlefieldHeight;
-      isAlive = true;
-      robotType = "JumpBot";
-      maxX = battlefieldWidth;
-      maxY = battlefieldHeight;
-      upgraded = true; // Already upgraded from GenericRobot
-    }
-}; */ 
+  public : 
+  jumpBot(string name, )
+}; 
