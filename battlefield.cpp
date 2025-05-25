e83f736dfb0b31d476f3706bf0df1a7b29d710d5
aniqahz <aniqahazhar45@gmail.com>
2025-05-25 18:29:02 +0800

    edit robot position inside battlefield.cpp

--- battlefield.cpp
+++ battlefield.cpp
@@ -36,10 +36,70 @@ bool config(ifstream& infile, int& row, int& col, int& steps)
     return true;
 }
 
-void robotPos(vector<vector<char>>& field, int x, int y, char sym)
+void robotPos(ifstream& infile, ofstream& outfile,vector<vector<char>>& field, int numRobot)
 {
-    if(x >= 0 && x < field.size() && y >= 0 && y < field[0].size())
+    string line;
+    for(int i=0; i<numRobot; ++i)
+    {
+        string rType, rName;
+        int x=-1, y=-1;
+
+        if(!getline(infile,line))
+        {
+            break;
+        }
+
+        char type[10], name[10];
+        rType = type;
+        rName = name;
+
+        size_t pos = line.find(rName);
+        string after = line.substr(pos + rName.length());
+        after.erase(0,after.find_first_not_of(' '));
+
+        if (after == "random random")
+        {
+            do
+            {
+                x = rand() % field.size();
+                y= rand() % field[0].size();
+
+            }
+            while (field[x][y] != '.');}
+        else
+        {
+            int nx, ny;
+            int read = sscanf(after.c_str(), "%d %d", &nx, &ny);
+            if(read !=2)
+            {
+                cout<<"invalid position for "<<rName<<endl;
+                outfile<<"invalid position for "<<rName<<endl;
+                continue;
+            }
+
+            x=nx;
+            y=ny;
+
+            if(x<0 || x>= field.size() || y<0 || y>= field[0].size())
+            {
+                cout<<"the position is out of bond"<<endl;
+                outfile<<"the position is out of bond"<<endl;
+                continue;
+            }
+
+            if(field[x][y] != '.')
+            {
+                cout<<"position is oocupied"<<endl;
+                outfile<<"position is oocupied"<<endl;   
+                continue;         
+            }
+        }
+        char sym = rName[0];
         field[x][y] = sym;
+
+        cout<<rName<<", "<<sym<<" placed at "<<x<<","<<y<<endl;
+        outfile<<rName<<", "<<sym<<" placed at "<<x<<","<<y<<endl;
+    }
 }
 
 void simulation(ofstream& outfile, vector<vector<char>>& field, int steps)
--- simulation.cpp
+++ simulation.cpp
@@ -18,71 +18,6 @@ int main()
 
     vector<vector<char>> field(row, vector<char>(col, '.'));
 
-    /*getline(infile, line); // steps
-    sscanf(line.c_str(), "steps: %d", &steps);
-
-    for (int i = 0; i < numRobot; ++i)
-    {
-        string rType, rName;
-        int x = -1, y = -1;
-        getline(infile, line);
-
-        cout << "robot line " << i + 1 << ": " << line << endl;
-
-        char type[20], name[20];
-        int ret = sscanf(line.c_str(), "%s %s", type, name);
-        if (ret != 2) {
-            cout << "Invalid robot format, skipping line" << endl;
-            continue;
-        }
-
-        rType = type;
-        rName = name;
-
-        size_t pos = line.find(rName);
-        string after = line.substr(pos + rName.length());
-        after.erase(0, after.find_first_not_of(' '));
-
-        if (after == "random random")
-        {
-            do {
-                x = rand() % row;
-                y = rand() % col;
-            } while (field[x][y] != '.');
-        }
-        else
-        {
-            int nx, ny;
-            int read = sscanf(after.c_str(), "%d %d", &nx, &ny);
-            if (read == 2)
-            {
-                x = nx;
-                y = ny;
-            }
-            else
-            {
-                cout << "Invalid position for " << rName << endl;
-                continue;
-            }
-
-            if (x < 0 || x >= row || y < 0 || y >= col)
-            {
-                cout << "Error: position out of bounds for " << rName << endl;
-                continue;
-            }
-
-            if (field[x][y] != '.')
-            {
-                cout << "Position (" << x << "," << y << ") is already occupied" << endl;
-                continue;
-            }
-        }
-
-        char sym = rName[0];
-        cout << rName << " (" << sym << ") is placed at (" << x << "," << y << ")" << endl;
-        robotPos(field, x, y, sym);
-    }*/
-
     int numRobot;
     string line;
     
@@ -92,7 +27,7 @@ int main()
     getline(infile, line); // robots
     sscanf(line.c_str(), "robots: %d", &numRobot);
 
-    robotPos(infile,outfile,field,numRobot);
+    robotPos(infile,outfile,field, numRobot);
     simulation(outfile, field, steps);
 
     infile.close();
