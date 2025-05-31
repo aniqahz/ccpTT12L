eb47e3021a3d1c87db4ae766b4817c6a8f04ad71
nuraleezdania <nuraleezdania05@gmail.com>
2025-06-01 00:28:50 +0800

    put the verification part

--- robot.cpp
+++ robot.cpp
@@ -1,3 +1,18 @@
+/**********|**********|**********|
+Program: robot.cpp
+Course: Object Oriented Programming and Data Structures
+Trimester: 2510
+Name: ANIQAH NABILAH BINTI AZHAR | JASMYNE YAP | 
+NUR ALEEZ DANIA BINTI MOHD SHAHRUL AZMAN | WAN HANANI IMAN BINTI WAN MOHD AZIDI @ SAPAWI
+ID: 242UC244LQ | 242UC244PT | 242UC244QB | 242UC244CK
+Lecture Section: TC3L
+Tutorial Section: T11L
+Email: aniqah.nabilah.azhar@student.mmu.edu.my | jasmyne.yap@student.mmu.edu.my |
+nur.aleez.dania@student.mmu.edu.my | wan.hanani.iman@student.mmu.edu.my
+Phone: 011-6204 6219 | 011-6346 4323 | 019-7109905 | 019-966 0664
+**********|**********|**********/
+
+
 //base class of robot
 #include "robot.h" 
 #include "battlefield.h"
@@ -240,7 +255,6 @@ void GenericRobot::move(int dx, int dy, vector<vector<char>>& field, ofstream& o
         log(cout, outfile, name + " cannot move — out of bounds.");
     }
 }
-
 
 //JUMP BOT-------------------------
 jumpBot::jumpBot(string name,  vector<vector<char>>&field, ofstream& outfile): GenericRobot(name,0,0), jumps(3){
